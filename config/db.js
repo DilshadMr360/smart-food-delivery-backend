ed128f5c6ca3fb9d204882ec41fd0f16e01655a9
DilshadMr360 <mohomeddilshad123@gmail.com>
2024-08-26 13:49:26 +0530

    backend fiels prtoected

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 import mangoose from "mongoose";
 
 export const connectDB =async ()=>{
-     await mangoose.connect('mongodb+srv://root:1234@cluster0.xz8du.mongodb.net/food-delivery-system').then(()=>console.log("DB Conncted"));
+     await mangoose.connect('').then(()=>console.log("DB Conncted"));
 }
